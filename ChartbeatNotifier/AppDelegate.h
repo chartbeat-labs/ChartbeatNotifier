f7ba344e39d0113f73b0d02d03a232b0b45cc775
Allan Beaufour <allan@chartbeat.com>
2012-06-04 18:07:36 -0400

    show cbtotal

--- ChartbeatNotifier/AppDelegate.h
+++ ChartbeatNotifier/AppDelegate.h
@@ -5,10 +5,20 @@
 
 #import <Cocoa/Cocoa.h>
 
+#import "SBJson.h"
+
 @interface AppDelegate : NSObject <NSApplicationDelegate> {
   NSWindow *window;
   IBOutlet NSMenu *statusMenu;
+
+  /** The status bar item */
   NSStatusItem *statusItem;
+
+  /** JSON parser used to part API results */
+  SBJsonParser *parser;
+
+  /** Timer used to periodically update the counter */
+  NSTimer *timer;
 }
 
 @property (assign) IBOutlet NSWindow *window;
--- ChartbeatNotifier/AppDelegate.m
+++ ChartbeatNotifier/AppDelegate.m
@@ -11,13 +11,47 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+  parser = [[SBJsonParser alloc] init];
+
+  timer = [NSTimer scheduledTimerWithTimeInterval:3.0
+                                           target:self selector:@selector(updateCounter:)
+                                         userInfo:nil
+                                          repeats:YES];
+  [self updateCounter:nil];
 }
 
--(void)awakeFromNib{
+-(void)awakeFromNib
+{
   statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
   [statusItem setMenu:statusMenu];
-  [statusItem setTitle:@"Status"];
+  [statusItem setTitle:@"(loading)"];
   [statusItem setHighlightMode:YES];
 }
+
+-(NSDictionary*)getJSON:(NSString*)aURL
+{
+  // TODO: add error handling galore
+  NSLog(@"getJSON(%@)", aURL);
+  NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:aURL]];
+  NSData *response = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+  NSString *json_string = [[NSString alloc] initWithData:response encoding:NSUTF8StringEncoding];
+  NSDictionary *data = [parser objectWithString:json_string error:nil];
+
+  return data;
+}
+
+-(void)updateCounter:(NSTimer*)aTimer
+{
+  NSLog(@"updateCounter()");
+
+  NSDictionary *data = [self getJSON:@"http://api.chartbeat.com/cbtotal"];
+  NSString *totaltotal = [data objectForKey:@"total"];
+
+  NSMutableString  *title = [NSMutableString stringWithFormat:@"Total: %@", totaltotal];
+  NSLog(@"%@", title);
+
+  [statusItem setTitle:title];
+}
+
 
 @end
